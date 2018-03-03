a144cc60bd38e79861f2c5493481be2c709f62b8
Dean Merchant <deanmerchant@gmail.com>
2018-03-03 15:08:20 +0000

    Experimenting with new context API

--- src/withNotificationSystem.ts
+++ src/withNotificationSystem.ts
@@ -15,19 +15,19 @@ export type Props = NotificationSystem.Attributes & {
 
 export type State = { notificationSystem: System | null }
 
-export class NotificationSystemProvider extends Component<Props, State> implements ChildContextProvider<ChildContext> {
-  static childContextTypes = { notificationSystem: PropTypes.any }
-  state = { notificationSystem: null }
+const NotificationContext = (React as any).createContext('notification-context')
 
-  getChildContext = (): ChildContext => ({ notificationSystem: this.state.notificationSystem })
+export class NotificationSystemProvider extends Component<Props, State>  {
+  state = { notificationSystem: null }
 
   saveSystemRef = (notificationSystem: System): void =>
     this.setState({ notificationSystem })
 
   render() {
     const { className, children, ...notificationSystemProps } = this.props
-    return createElement('div', {
+    return createElement(NotificationContext.Provider, {
       className: className || 'NotificationSystemProvider',
+      value: this.state.notificationSystem,
       children: [
         ...[this.state.notificationSystem == null ? undefined : children],
         createElement(NotificationSystem, {
@@ -54,38 +54,31 @@ export type NotificationProps = {
 
 export const withNotifications = <OP>(WrappedComponent: ComponentType<OP & NotificationProps>): ComponentClass<OP> =>
   class NotificationsWrapper extends Component<OP> {
-    static contextTypes = { notificationSystem: PropTypes.any }
     displayName: string = `withNotifications(${WrappedComponent.displayName || WrappedComponent.name})`
-    notificationSystem: System = (null as any) // tslint:disable-line:no-any
-
-    componentWillMount() {
-      if (this.context.notificationSystem == null) {
-        throw new Error('Component must be a descendant of a <NotificationSystemProvider /> element')
-      } 
-      this.notificationSystem = this.context.notificationSystem
-    }
-
-    addNotification = (notification: Notification): ResultNotification => {
-      const level = notification.level || 'success'
-      const title = notification.title || level.charAt(0).toUpperCase() + level.substr(1)
-      return this.notificationSystem.addNotification({
-        position: 'tr',
-        ...notification,
-        level,
-        title,
-      }) as ResultNotification
-    }
 
     render() {
-      const enhancedProps: OP & NotificationProps = Object.assign({}, this.props, { notification: {
-        add: this.addNotification,
-        remove: this.notificationSystem.removeNotification,
-        removeAll: this.notificationSystem.clearNotifications,
+      return createElement(NotificationContext.Consumer, { children: (notificationSystem: System) => {
+        if (notificationSystem == null) {
+          throw new Error('Component must be a descendant of a <NotificationSystemProvider /> element')
+        }
+        const enhancedProps: OP & NotificationProps = {
+          ...(this.props as {}),
+          notification: {
+            add: (notification: Notification): ResultNotification => {
+              const level = notification.level || 'success'
+              const title = notification.title || level.charAt(0).toUpperCase() + level.substr(1)
+              return notificationSystem.addNotification({
+                position: 'tr',
+                ...notification,
+                level,
+                title,
+              }) as ResultNotification
+            },
+            remove: notificationSystem.removeNotification,
+            removeAll: notificationSystem.clearNotifications,
+          },
+        } as OP & NotificationProps
+        return createElement(WrappedComponent as ComponentClass<OP & NotificationProps>, enhancedProps)
       }})
-
-      return createElement(
-        WrappedComponent as ComponentClass<OP & NotificationProps>,
-        enhancedProps,
-      )
     }
   }
